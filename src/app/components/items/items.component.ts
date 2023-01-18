abb629776eb7aa3147054a990389e565019cdc40
illika <steve.milan7@gmail.com>
2023-01-17 23:18:20 -0500

    componente y clase

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Item } from 'src/models/Item';
+
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
+  items: Item[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        id: 0,
+        title: "manzana",
+        price: 8.25,
+        quantity: 4,
+        completed: false
+      },
+      {
+        id: 1,
+        title: "platano",
+        price: 4.5,
+        quantity: 2,
+        completed: false
+      },
+      {
+        id: 2,
+        title: "naranja",
+        price: 14,
+        quantity: 5,
+        completed: true
+      }
+    ]
   }
 
 }
