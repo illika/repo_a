02b6d6ae650ec460f4648cd0e511dd41b825b175
illika <steve.milan7@gmail.com>
2023-01-17 23:27:31 -0500

    @Input()

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from 'src/models/Item';
 
 @Component({
   selector: 'app-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+
+  @Input() item: Item = new Item();
 
   constructor() { }
 
